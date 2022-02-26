17a2eb2719ea76a240a4285f6259c7c3e5b18ed5
Yana Kosteva <yanakosteva.3399@gmail.com>
2022-02-26 22:33:00 +0700

    Add geometry0

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello, World");
+    FILE* TXT;
+    char n[20];
+    char* an;
+    TXT = fopen("input.txt", "r");
+    if (TXT == 0) {
+        printf("Error\n");
+        return 0;
+    } else
+        printf("line reading:\n");
+    do {
+        an = fgets(n, sizeof(n), TXT);
+        if (an == NULL) {
+            if (getc(TXT) != 0) {
+                printf("Reading a file 'input' end\n");
+                break;
+            } else {
+                printf("\nFile error 'input'\n");
+                break;
+            }
+        }
+        printf("%s", n);
+    } while (1);
+    printf("Result:");
+    if (fclose(TXT) == 1)
+        printf("Error\n");
+    else
+        printf("Complete\n");
     return 0;
 }
