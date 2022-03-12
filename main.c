a06ba43277ebf42972b7032a75dcb9c26bb7eeaa
Yana Kosteva <yanakosteva.3399@gmail.com>
2022-03-12 22:52:31 +0700

    Fixed main.c

--- main.c
+++ main.c
@@ -1,32 +1,104 @@
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <string.h>
+int flag1 = 0, flag2 = 0;
+
+int checkfigure(char strf[])
 {
-    FILE* TXT;
-    char n[20];
-    char* an;
-    TXT = fopen("input.txt", "r");
-    if (TXT == 0) {
-        printf("Error\n");
-        return 0;
-    } else
-        printf("line reading:\n");
-    do {
-        an = fgets(n, sizeof(n), TXT);
-        if (an == NULL) {
-            if (getc(TXT) != 0) {
-                printf("Reading a file 'input' end\n");
-                break;
+    char figure[] = "circle", *end;
+    int i = 0, k = 0, kr, flag1 = 0, flag2 = 0;
+    double x, y, r;
+    if (strncmp(strf, figure, 6) == 0) {
+        if (strf[6] == '(') {
+            flag1 = 1;
+        } else {
+            if (strf[6] == ' ') {
+                printf("\nError: unexpected space");
+                return 1;
             } else {
-                printf("\nFile error 'input'\n");
-                break;
+                printf("\nError: wrong name figure");
+                return 2;
             }
         }
-        printf("%s", n);
-    } while (1);
-    printf("Result:");
-    if (fclose(TXT) == 1)
-        printf("Error\n");
-    else
-        printf("Complete\n");
+    } else {
+        printf("\nError: wrong name figure");
+        return 3;
+    }
+
+    if (flag1 == 1) {
+        i = 7;
+        while (strf[i] != ')' && strf[i] != '\0') {
+            if (strf[i - 1] == '(' && strf[i] == ' ') {
+                printf("\nError: unexpected space after '('");
+                return 9;
+            }
+            if (flag2 == 0 && (isdigit(strf[i]) != 0 || strf[i] == '-' || strf[i] == '.')) {
+                i++;
+                if (strf[i] == ' ') {
+                    flag2++;
+                    i++;
+                }
+            }
+            if (flag2 == 1 && (isdigit(strf[i]) != 0 || strf[i] == '-' || strf[i] == '.')) {
+                i++;
+                if (strf[i] == ',' && strf[i + 1] == ' ' && strf[i + 2] != '-') {
+                    flag2++;
+                    kr = i + 1;
+                    i += 2;
+                }
+                if (strf[i] == ',' && strf[i + 1] != ' ') {
+                    printf("\nError: expected space after ','");
+                    return 4;
+                }
+                if (strf[i] == ',' && strf[i + 2] == '-') {
+                    printf("\nError: unexpected '-'");
+                    return 13;
+                }
+            }
+            if (flag2 == 2 && (isdigit(strf[i]) != 0 || strf[i] == '.')) {
+                i++;
+                if (strf[i] == ')') {
+                    flag2++;
+                }
+            }
+            if (isdigit(strf[i]) == 0 && strf[i] != '-' && strf[i] != '.' && strf[i] != ',' && strf[i] != ' ' && strf[i] != ')') {
+                printf("\nError: unexpected token %d", i);
+                return 5;
+            }
+            if (strf[i] == ')')
+                continue;
+        }
+        if (flag2 == 3 && strf[i] == ')' && (strf[i + 1] == '\0' || strf[i + 1] == ' ')) {
+            flag2++;
+        } else {
+            if (flag2 == 3 && strf[i] == ')' && strf[i + 1] != '\0' && strf[i + 1] != ' ') {
+                printf("\nError: unexpected token after ')'");
+                return 6;
+            }
+            if (flag2 != 3 && strf[i] == ')') {
+                printf("\nError: wrong parametrs");
+                return 7;
+            }
+        }
+    }
+    if (flag2 == 4) {
+        char *parx, *pary, *parrad, *end;
+        printf("\nFigure: %s\n", figure);
+        parx = &strf[7];
+        parrad = &strf[kr];
+        printf("x=%.2f\n", strtod(parx, &pary));
+        printf("y=%.2f\n", strtod(pary, &end));
+        printf("radius=%.2f\n", strtod(parrad, &end));
+    }
+}
+
+int main()
+{
+    char strf[50];
+    printf("Enter figure and parameters: ");
+    gets(strf);
+    checkfigure(strf);
     return 0;
 }
