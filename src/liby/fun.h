b824267680d94c485e56a1be9fbb831c076223ac
Yana Kosteva <yanakosteva.3399@gmail.com>
2022-05-15 00:55:08 +0700

    Fixed bugs2

--- src/liby/fun.h
+++ src/liby/fun.h
@@ -2,7 +2,20 @@
 
 double S_circle(double r);
 double P_circle(double r);
-int checkfigure(char strf[], int n, double xfig[],double yfig[], double rfig[], double Sfig[], double Pfig[]);
-void intersect(int j, int n, double xfig[],double yfig[], double rfig[]);
-void print(int j, double xfig[],double yfig[], double rfig[], double Sfig[], double Pfig[]);
+int checkfigure(
+        char strf[],
+        int n,
+        double xfig[],
+        double yfig[],
+        double rfig[],
+        double Sfig[],
+        double Pfig[]);
+void intersect(int j, int n, double xfig[], double yfig[], double rfig[]);
+void print(
+        int j,
+        double xfig[],
+        double yfig[],
+        double rfig[],
+        double Sfig[],
+        double Pfig[]);
 void flush_input();
